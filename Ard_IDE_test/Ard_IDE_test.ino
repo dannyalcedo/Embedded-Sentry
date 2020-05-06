4cd69e6309d38696124716b9f890452f0eba436a
Danny <alcedodanny2402@gmail.com>
2020-05-06 16:50:07 -0400

    Added ArduinoIDE tester file and starting to figure out quantiziation

--- Ard_IDE_test/Ard_IDE_test.ino
+++ Ard_IDE_test/Ard_IDE_test.ino
@@ -24,7 +24,7 @@ static unsigned long lastPrint = 0; // Keep track of print time
 void printGyro();
 void printAccel();
 void printMag();
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
+void printAttitude(float axS, float ayS, float azS, float mxS, float myS, float mzS);
 
 float accelPre[3][10]; //2D array for smoothing accel values
 float magPre[3][10]; //2D array for smoothing mag values
@@ -95,18 +95,15 @@ void loop()
       avgMag[i] = (magPre[i][0] + magPre[i][1] + magPre[i][2]+magPre[i][3] + magPre[i][4] + magPre[i][5]+magPre[i][6] + magPre[i][7] + magPre[i][8]+magPre[i][9])/10;
     }
 
-    printGyro();  // Print "G: gx, gy, gz"
-    printAccel(); // Print "A: ax, ay, az"
-    printMag();   // Print "M: mx, my, mz"
     // Print the heading and orientation for fun!
     // Call print attitude. The LSM9DS1's mag x and y
     // axes are opposite to the accelerometer, so my, mx are
     // substituted for each other.
-    //printAttitude(avgAccel[0], avgAccel[1], avgAccel[2],
-                  //-avgMag[0], -avgMag[1], avgMag[2]);
-                  
-    printAttitude(imu.ax, imu.ay, imu.az,
-                 -imu.my, -imu.mx, imu.mz);
+//    printAttitude(avgAccel[0], avgAccel[1], avgAccel[2], -avgMag[0], -avgMag[1], avgMag[2]);
+                
+//    printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+
+    printAttitude(avgAccel[0], avgAccel[1], avgAccel[2], -avgMag[0], -avgMag[1], avgMag[2]);
 
     Serial.println();
 
@@ -114,109 +111,81 @@ void loop()
   }
 }
 
-void printGyro()
-{
-  // Now we can use the gx, gy, and gz variables as we please.
-  // Either print them as raw ADC values, or calculated in DPS.
-  Serial.print("G: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcGyro helper function to convert a raw ADC value to
-  // DPS. Give the function the value that you want to convert.
-  Serial.print(imu.calcGyro(imu.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
-  Serial.println(" deg/s");
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);
-  Serial.print(", ");
-  Serial.print(imu.gy);
-  Serial.print(", ");
-  Serial.println(imu.gz);
-#endif
-}
 
-void printAccel()
-{
-  // Now we can use the ax, ay, and az variables as we please.
-  // Either print them as raw ADC values, or calculated in g's.
-  Serial.print("A: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcAccel helper function to convert a raw ADC value to
-  // g's. Give the function the value that you want to convert.
-  Serial.print(imu.calcAccel(imu.ax), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
-  Serial.println(" g");
-#elif defined PRINT_RAW
-  Serial.print(imu.ax);
-  Serial.print(", ");
-  Serial.print(imu.ay);
-  Serial.print(", ");
-  Serial.println(imu.az);
-#endif
-
-}
-
-void printMag()
-{
-  // Now we can use the mx, my, and mz variables as we please.
-  // Either print them as raw ADC values, or calculated in Gauss.
-  Serial.print("M: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcMag helper function to convert a raw ADC value to
-  // Gauss. Give the function the value that you want to convert.
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mz), 2);
-  Serial.println(" gauss");
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);
-  Serial.print(", ");
-  Serial.print(imu.my);
-  Serial.print(", ");
-  Serial.println(imu.mz);
-#endif
-}
 
 // Calculate pitch, roll, and heading.
 // Pitch/roll calculations take from this app note:
 // http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
 // Heading calculations taken from this app note:
 // http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+void printAttitude(float axS, float ayS, float azS, float mxS, float myS, float mzS)
 {
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
 
-  float heading;
-  if (my == 0)
-    heading = (mx < 0) ? PI : 0;
+  //for smoothed
+  float rollS = atan2(ayS, azS);
+  float pitchS = atan2(-axS, sqrt(ayS * ayS + azS * azS));
+  float quantPitch;
+  float quantRoll;
+
+  float headingS;
+  float quantHeading;
+  if (myS == 0)
+    headingS = (mxS < 0) ? PI : 0;
   else
-    heading = atan2(mx, my);
+    headingS = atan2(mxS, myS);
 
-  heading -= DECLINATION * PI / 180;
+  headingS -= DECLINATION * PI / 180;
 
-  if (heading > PI) heading -= (2 * PI);
-  else if (heading < -PI) heading += (2 * PI);
+  if (headingS > PI) headingS -= (2 * PI);
+  else if (headingS < -PI) headingS += (2 * PI);
 
   // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll  *= 180.0 / PI;
+  headingS *= 180.0 / PI;
+  pitchS *= 180.0 / PI;
+  rollS  *= 180.0 / PI;
 
-  // Print
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
-  Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
+  //quantizing pitch
+  if ((pitchS < 90) && (pitchS > 60)){
+    quantPitch = 1;
+  }
+  else if ((pitchS < -60) && (pitchS > -90)){
+    quantPitch = -1;
+  }
+  else {
+    quantPitch = 0;
+  }
+
+  //quantizing roll
+  if ((rollS < 105) && (rollS > 70)){
+    quantRoll = 1;
+  }
+  else if ((rollS < -75) && (rollS > -105)){
+    quantRoll = -1;
+  }
+  else {
+    quantRoll = 0;
+  }
+
+  //quantizing heading (yaw)
+  if ((headingS < 5) && (headingS > -25)){
+    quantHeading = 1;
+  }
+  else if ((headingS < -140) && (headingS > -160)){
+    quantHeading = -1;
+  }
+  else {
+    quantHeading = 0;
+  }
+//  Serial.print(pitchS, 2);
+//  Serial.print(" ");
+//  Serial.print(rollS, 2);
+//  Serial.print(" ");
+//  Serial.print(headingS, 2);
+//  Serial.print(" ");
+
+  Serial.print(quantPitch, 2);
+  Serial.print(" ");
+  Serial.print(quantRoll, 2);
+  Serial.print(" ");
+  Serial.print(quantHeading, 2);
 }
